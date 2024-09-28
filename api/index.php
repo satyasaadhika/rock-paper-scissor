1fff778132cf7c4d1cf43e36e9253bc1861cd906
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:32:00 +0000

    Use PATH_INFO instead of REQUEST_URI

--- api/index.php
+++ api/index.php
@@ -1,9 +1,6 @@
 <?php
-if (str_contains($_SERVER['REQUEST_URI'], 'game.php')) {
-    echo $_SERVER['REQUEST_URI'];
-    require __DIR__ . "/..".$_SERVER['REQUEST_URI'];
-} else if (str_contains($_SERVER['REQUEST_URI'], '.php')) {
-    require __DIR__ . "/../".$_SERVER['REQUEST_URI'];
+if (str_contains($_SERVER['PATH_INFO'], '.php')) {
+    require __DIR__ . "/../".$_SERVER['PATH_INFO'];
 } else {
     require __DIR__ . "/../index.php";
 }
