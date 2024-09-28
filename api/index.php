14e4e8c63fc21a47686858f0efa9c6bb0347db10
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:27:54 +0000

    Write separate condition for game.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,8 @@
 <?php
-
-if (str_contains($_SERVER['REQUEST_URI'], '.php')) {
+echo $_SERVER['REQUEST_URI'];
+if (str_contains($_SERVER['REQUEST_URI'], 'game.php')) {
+    require __DIR__ . "/..".$_SERVER['REQUEST_URI'];
+} else if (str_contains($_SERVER['REQUEST_URI'], '.php')) {
     require __DIR__ . "/../".$_SERVER['REQUEST_URI'];
 } else {
     require __DIR__ . "/../index.php";
