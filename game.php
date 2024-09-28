3539e4a832ecf379cd0bb15fcb4568f827cef91b
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:57:00 +0000

    Remove obsolete template content

--- game.php
+++ game.php
@@ -1,12 +1,12 @@
 <?php
 
 // Demand a GET parameter
-if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
+if (! isset($_GET['name']) || strlen($_GET['name']) < 1) {
     die('Name parameter missing');
 }
 
 // If the user requested logout go back to index.php
-if ( isset($_POST['logout']) ) {
+if (isset($_POST['logout'])) {
     header('Location: index.php');
     return;
 }
@@ -14,17 +14,19 @@ if ( isset($_POST['logout']) ) {
 // Set up the values for the game...
 // 0 is Rock, 1 is Paper, and 2 is Scissors
 $names = array('Rock', 'Paper', 'Scissors');
-$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
+$human = isset($_POST["human"]) ? $_POST['human'] + 0 : -1;
 
-$computer = 0; 
-$computer = rand(0,2);
+$computer = 0;
+$computer = rand(0, 2);
 
-function check($computer, $human) {
-    if ( $human == $computer ) {
+function check($computer, $human)
+{
+    if ($human == $computer) {
         return "Tie";
-    } else if ( ($human == 0 && $computer == 1) ||
-                ($human == 1 && $computer == 2) ||
-                ($human == 2 && $computer == 0)) {
+    } else if (($human == 0 && $computer == 1) ||
+        ($human == 1 && $computer == 2) ||
+        ($human == 2 && $computer == 0)
+    ) {
         return "You Lose";
     } else {
         return "You Win";
@@ -38,39 +40,41 @@ $result = check($computer, $human);
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Priyanshu Maurya Rock, Paper, Scissors Game - 56d7e76b </title>
-<?php require_once "bootstrap.php"; ?>
-</head>
-<body>
-<div class="container">
-<h1>Rock Paper Scissors</h1>
-<?php
-if ( isset($_REQUEST['name']) ) {
-    echo "<p>Welcome: ";
-    echo htmlentities($_REQUEST['name']);
-    echo "</p>\n";
-}
-?>
-<form method="post">
-<select name="human">
-<option value="-1">Select</option>
-<option value="0">Rock</option>
-<option value="1">Paper</option>
-<option value="2">Scissors</option>
-<option value="3">Test</option>
-</select>
-<input type="submit" value="Play">
-<input type="submit" name="logout" value="Logout">
-</form>
 
-<pre>
+<head>
+    <title>Priyanshu Maurya Rock, Paper, Scissors Game - 56d7e76b </title>
+    <?php require_once "bootstrap.php"; ?>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Rock Paper Scissors</h1>
+        <?php
+        if (isset($_REQUEST['name'])) {
+            echo "<p>Welcome: ";
+            echo htmlentities($_REQUEST['name']);
+            echo "</p>\n";
+        }
+        ?>
+        <form method="post">
+            <select name="human">
+                <option value="-1">Select</option>
+                <option value="0">Rock</option>
+                <option value="1">Paper</option>
+                <option value="2">Scissors</option>
+                <option value="3">Test</option>
+            </select>
+            <input type="submit" value="Play">
+            <input type="submit" name="logout" value="Logout">
+        </form>
+
+        <pre>
 <?php
-if ( $human == -1 ) {
+if ($human == -1) {
     print "Please select a strategy and press Play.\n";
-} else if ( $human == 3 ) {
-    for($c=0;$c<3;$c++) {
-        for($h=0;$h<3;$h++) {
+} else if ($human == 3) {
+    for ($c = 0; $c < 3; $c++) {
+        for ($h = 0; $h < 3; $h++) {
             $r = check($c, $h);
             print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
         }
@@ -80,6 +84,7 @@ if ( $human == -1 ) {
 }
 ?>
 </pre>
-</div>
+    </div>
 </body>
+
 </html>
