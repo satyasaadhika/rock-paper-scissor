28ee626b15b6ce4f86809b723231f7f91523d0be
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:44:34 +0000

    Use PATH_TRANSLATED instead of PATH_INFO

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-if (str_contains($_SERVER['PATH_INFO'], '.php')) {
-    require __DIR__ . "/../".$_SERVER['PATH_INFO'];
+if (str_contains($_SERVER['PATH_TRANSLATED'], '.php')) {
+    require __DIR__ . "/../".$_SERVER['PATH_TRANSLATED'];
 } else {
     require __DIR__ . "/../index.php";
 }
