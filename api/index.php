3f9c0d8e2089bcd9c28563eed4f3850494405236
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:29:58 +0000

    Print request URI on game page

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-echo $_SERVER['REQUEST_URI'];
 if (str_contains($_SERVER['REQUEST_URI'], 'game.php')) {
+    echo $_SERVER['REQUEST_URI'];
     require __DIR__ . "/..".$_SERVER['REQUEST_URI'];
 } else if (str_contains($_SERVER['REQUEST_URI'], '.php')) {
     require __DIR__ . "/../".$_SERVER['REQUEST_URI'];
