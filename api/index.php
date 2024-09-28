3c118e1d3e554308a191ff0847efd89511efc998
Priyanshu Maurya <106907193+satyasaadhika@users.noreply.github.com>
2024-09-28 17:53:53 +0000

    Use REQUEST_URI with preg_replace to get path

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-if (str_contains($_SERVER['PATH_TRANSLATED'], '.php')) {
-    require __DIR__ . "/../".$_SERVER['PATH_TRANSLATED'];
+if (str_contains($_SERVER['REQUEST_URI'], '.php')) {
+    require __DIR__ . "/../".preg_replace("/\?.*\z/", "", $_SERVER['REQUEST_URI']);
 } else {
     require __DIR__ . "/../index.php";
 }
